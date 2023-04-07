4f5907ca8e3867323f027d9be21944b476a024f6
Surya Ashish <ashishsurya126@gmail.com>
2023-04-07 10:45:40 +0530

    Update parseAuthToken.ts

--- utils/parseAuthToken.ts
+++ utils/parseAuthToken.ts
@@ -1,3 +1,2 @@
 export const parseAuthToken = (str: string) => str.split(';')[0].split('=')[1];
 
-// str = authToken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImpvaG5kb2UiLCJzdWIiOiIyNjY0Y2I2Mi1hMTlkLTQxYzAtODYwOC0zYjUyN2Y4NmNiM2IiLCJpYXQiOjE2ODA3MjM3NTksImV4cCI6MTY4MDgxMDE1OX0.ferlcoizUWA9ZgXqltRfjQiTrr_zGvhBdoxnb-gc3CM; Path=/; Expires=Thu, 06 Apr 2023 19:42:39 GMT; HttpOnly; Secure; SameSite=None
